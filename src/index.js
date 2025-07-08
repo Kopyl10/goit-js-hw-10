3198484e862253d893fb2df28d0e45689575ec3c
Kopyl10 <kopyl10@gmail.com>
2025-07-07 22:48:16 -0700

    3432

--- src/index.js
+++ src/index.js
@@ -1,56 +1,90 @@
-import './sass/index.scss';
 import { fetchBreeds, fetchCatByBreed } from './cat-api.js';
 import SlimSelect from 'slim-select';
 import Notiflix from 'notiflix';
+import './styles.css';
 
 const breedSelect = document.querySelector('.breed-select');
 const catInfo = document.querySelector('.cat-info');
 const loader = document.querySelector('.loader');
 const errorText = document.querySelector('.error');
 
-breedSelect.classList.add('is-hidden');
-catInfo.classList.add('is-hidden');
-errorText.classList.add('is-hidden');
+function showLoader() {
+  loader.classList.remove('is-hidden');
+}
+
+function hideLoader() {
+  loader.classList.add('is-hidden');
+}
+
+function showError() {
+  errorText.classList.remove('is-hidden');
+}
+
+function hideError() {
+  errorText.classList.add('is-hidden');
+}
+
+function showCatInfo() {
+  catInfo.classList.remove('is-hidden');
+}
+
+function hideCatInfo() {
+  catInfo.classList.add('is-hidden');
+}
+
+function showSelect() {
+  breedSelect.classList.remove('is-hidden');
+}
+
+function hideSelect() {
+  breedSelect.classList.add('is-hidden');
+}
+
+hideSelect();
+hideCatInfo();
+hideError();
+showLoader();
+
 fetchBreeds()
   .then(breeds => {
     breedSelect.innerHTML = breeds
       .map(breed => `<option value="${breed.id}">${breed.name}</option>`)
       .join('');
     new SlimSelect({ select: '.breed-select' });
-    loader.classList.add('is-hidden');
-    breedSelect.classList.remove('is-hidden');
+    showSelect();
+    hideLoader();
   })
   .catch(() => {
-    loader.classList.add('is-hidden');
-    errorText.classList.remove('is-hidden');
-    Notiflix.Notify.failure(
-      'Oops! Something went wrong. Try reloading the page!'
-    );
+    hideLoader();
+    showError();
+    Notiflix.Notify.failure('Failed to load breeds. Try again.');
   });
+
 breedSelect.addEventListener('change', event => {
   const breedId = event.target.value;
-
-  catInfo.classList.add('is-hidden');
-  loader.classList.remove('is-hidden');
-  errorText.classList.add('is-hidden');
+  showLoader();
+  hideCatInfo();
+  hideError();
 
   fetchCatByBreed(breedId)
     .then(data => {
       const { url, breeds } = data;
+      const { name, description, temperament } = breeds[0];
+
       catInfo.innerHTML = `
-        <img src="${url}" alt="${breeds[0].name}" width="400" />
+        <img src="${url}" alt="${name}" width="400" />
         <div>
-          <h2>${breeds[0].name}</h2>
-          <p>${breeds[0].description}</p>
-          <p><strong>Temperament:</strong> ${breeds[0].temperament}</p>
+          <h2>${name}</h2>
+          <p>${description}</p>
+          <p><strong>Temperament:</strong> ${temperament}</p>
         </div>
       `;
-      loader.classList.add('is-hidden');
-      catInfo.classList.remove('is-hidden');
+      showCatInfo();
+      hideLoader();
     })
     .catch(() => {
-      loader.classList.add('is-hidden');
-      errorText.classList.remove('is-hidden');
-      Notiflix.Notify.failure('Oops! Something went wrong. Please try again.');
+      hideLoader();
+      showError();
+      Notiflix.Notify.failure('Failed to load cat data.');
     });
 });
